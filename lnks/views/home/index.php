58c9b616dd4dbfc265fb4dcdbf938b50b4ca7109
Tim Kächele <TimKaechele@me.com>
2014-06-13 23:58:37 +0200

    Update index.php

--- lnks/views/home/index.php
+++ lnks/views/home/index.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <title>
-    lnks
-  </title>
+  <meta charset="UTF-8">
+  <title>lnks</title>
+  <link rel="stylesheet" href="<?php echo stylesheet_path('css/app.min.css') ?>">
+  <script type="text/javascript" src="<?php echo javascript_path('js/app.min.js') ?>"></script>
 </head>
 <body>
-  <?php echo "hello world this is my home index view" ?>
+  <?php foreach($templates as $template): ?>
+    <script type="text/x-handlebars" id="<?php echo $template->id() ?>">
+      <?php echo $template->content() ?>
+    </script>  
+  <?php endforeach ?>
 </body>
 </html>
