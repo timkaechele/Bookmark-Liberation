01eb558efbda8ac2415d3aa4d1ee8113a5cb3edb
Tim Kächele <TimKaechele@me.com>
2014-06-14 00:05:05 +0200

    Create healthcheck.php

---
+++ lnks/helpers/healthcheck.php
@@ -0,0 +1,6 @@
+<?php
+  class HealthCheck {
+    public static function is_data_writable(){}
+    public static function is_database_existent(){}
+    public static function is_database_accessible(){}
+  }
